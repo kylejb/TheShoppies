cf1fdefda312ca12f070c1b246127f7806ac3604
kylejb <47502769+kylejb@users.noreply.github.com>
2020-12-20 21:49:23 -0500

    Minor formatting.

--- src/SearchSuggestions.tsx
+++ src/SearchSuggestions.tsx
@@ -1,8 +1,10 @@
 import MovieCard from "./MovieCard";
 
+
 const SearchSuggestions = (props: any) => {
     const options = props.results?.map( (m: any) => <MovieCard key={m.imdbID} movie={m} />);
 
+    
     return (
         <div>
             {options}
