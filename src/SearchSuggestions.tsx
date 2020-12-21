30f33bf681c5a817486e018aa14da78e478ceab2
kylejb <47502769+kylejb@users.noreply.github.com>
2020-12-20 21:51:35 -0500

    Minor reformatting for readability.

--- src/SearchSuggestions.tsx
+++ src/SearchSuggestions.tsx
@@ -2,9 +2,14 @@ import MovieCard from "./MovieCard";
 
 
 const SearchSuggestions = (props: any) => {
-    const options = props.results?.map( (m: any) => <MovieCard key={m.imdbID} movie={m} />);
+    const options = props.results?.map( (m: any) => (
+        <MovieCard 
+            key={m.imdbID} 
+            movie={m} 
+        />)
+    );
 
-    
+
     return (
         <div>
             {options}
