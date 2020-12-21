5fb856375a584b795b99a0689ea69b9790652adf
kylejb <47502769+kylejb@users.noreply.github.com>
2020-12-20 21:28:25 -0500

    Wiring up foundation for Search based interface.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-function App() {
+import Search from './Search';
+
+
+const App = () => {
     return (
-        <div className="App">
-            <p>App Component</p>
+        <div className="app">
+            <h1>The Shoppies</h1>
+            <Search />
         </div>
     );
-}
+};
 
 export default App;
---
+++ src/Search.tsx
@@ -0,0 +1,55 @@
+import { useContext, useEffect, useState } from 'react';
+import { SearchContext } from './SearchContext';
+import Dropdown from './Dropdown';
+
+
+const BASE_API_EP = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
+
+
+interface IResults {
+    Search: Array<String>;  
+};
+
+
+const Search = () => {
+    const [query, setQuery] = useState(""),
+        [results, setResults] = useState<IResults[]>([]);
+          
+    const searchctx = useContext(SearchContext),
+        { setSearchState } = searchctx;
+
+    const inputChangeHandler = (e: React.BaseSyntheticEvent) => {
+        setQuery(e.target.value);
+    };
+
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            const response = await fetch(`${BASE_API_EP}&type=movie&s=${query}`);
+            const json = await response.json();
+
+            switch (json.Response) {
+                case "True":
+                    setResults(json.Search);
+                    break; 
+                default:
+                    //* DEFAULT SWITCH >> Render Msg for user on page
+                    break;
+            };
+        };
+        if (query.length >= 3) {
+            setSearchState({isSearching: true});
+            fetchMovies();
+        };
+    }, [query, setResults, setSearchState]);
+
+    
+    return (
+        <form>
+            <input value={query} onChange={inputChangeHandler} />
+            <Dropdown results={results} />
+        </form>
+    );
+};
+
+export default Search;
