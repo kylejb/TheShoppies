9839123ace5af57c7b69dac7741646ceb58c8997
kylejb <47502769+kylejb@users.noreply.github.com>
2020-12-21 11:59:21 -0500

    Wired up basic components to house nominated results.

---
+++ src/NominationContainer.tsx
@@ -0,0 +1,12 @@
+import NominationResults from "./NominationResults"
+
+const NominationContainer = () => {
+    return (
+        <div className="container">
+            <h1>Nomination Container</h1>
+            <NominationResults />
+        </div>
+    );
+};
+
+export default NominationContainer;
---
+++ src/NominationResults.tsx
@@ -0,0 +1,7 @@
+const NominationResults = () => {
+    return (
+        <h1>Nomination Results</h1>
+    );
+};
+
+export default NominationResults;
