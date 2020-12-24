bb05eead4510e61f2fa0cbccb8feb78bc0cb47d6
kylejb <47502769+kylejb@users.noreply.github.com>
2020-12-24 18:35:30 -0500

    Minor formatting

--- src/containers/NominationContainer.tsx
+++ src/containers/NominationContainer.tsx
@@ -3,9 +3,6 @@ import "../styles/containers/NominationContainer/style.scss";
 
 
 const NominationContainer = () => {
-
-
-
     return (
         <div className="wrapper-nomination">
             <h1>Nomination Container</h1>
