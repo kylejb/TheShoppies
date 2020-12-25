d162fd7ce65596437ddb5e31aaa367e0a2f3624f
kylejb <47502769+kylejb@users.noreply.github.com>
2020-12-25 18:26:17 -0500

    Added an async action (i.e., getMovies via API).

--- src/redux/actionCreators.ts
+++ src/redux/actionCreators.ts
@@ -1,7 +1,30 @@
-import { ADD_MOVIE } from "./actionTypes";
+import { ADD_MOVIE, FETCH_MOVIES } from "./actionTypes";
 
 
 export const addMovie = ( content: IMovie ) => ({
     type: ADD_MOVIE,
     payload: content
 });
+
+
+const BASE_API_EP = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
+
+export const getMovies = () => {
+    return async function ( dispatch: any ) {
+        let response = await fetch(`${BASE_API_EP}&type=movie&s=${"star"}`);
+        let json = await response.json();
+        dispatch({ type: FETCH_MOVIES, payload: json });            
+    };
+};
+
+
+// switch (json.Response) {
+//     case "True":
+//         setResults(json.Search);
+//         setTotalResults(json.totalResults);
+//         break; 
+//     default:
+//         setTotalResults("0");
+//         //* DEFAULT SWITCH >> Render Msg for user on page
+//         break;
+// };
--- src/redux/actionTypes.ts
+++ src/redux/actionTypes.ts
@@ -1,2 +1,3 @@
 export const ADD_MOVIE = "ADD_MOVIE";
-export const REMOVE_MOVIE = "REMOVE_MOVIE"
+export const REMOVE_MOVIE = "REMOVE_MOVIE";
+export const FETCH_MOVIES = "FETCH_MOVIES";
