59dfc1b30e9c5fbfcefdcc0ab41c63b2bf2eeb20
kylejb <47502769+kylejb@users.noreply.github.com>
2020-12-25 18:23:54 -0500

    Added thunk middleware to handle async actions.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import App from "./App";
 import "./styles/base.scss";
@@ -21,7 +22,7 @@ const rootReducer = (currentState: any = {nominated: []}, action: any) => {
     };
 };
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 
 ReactDOM.render(
