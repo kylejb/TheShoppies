9ba0478226c729d11d4b75c8b27a231139745b88
kylejb <47502769+kylejb@users.noreply.github.com>
2020-12-25 10:36:37 -0500

    Initialized Redux store and made it accessible to App via Provider (HOC).

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import App from "./App";
-import { SearchProvider } from "./SearchContext";
+// import { SearchProvider } from "./SearchContext";
 import "./styles/base.scss";
 
 
+const rootReducer = () => { return { nominated: [] }}
+
+const store = createStore(rootReducer);
+
+console.log("STORE", store.getState())
+
 ReactDOM.render(
     <React.StrictMode>
-        <SearchProvider>
+        <Provider store={store}>
+            {/* <SearchProvider> */}
             <App />
-        </SearchProvider>
+            {/* </SearchProvider> */}
+        </Provider>
     </React.StrictMode>,
     document.getElementById("root")
 );
