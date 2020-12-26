43ac246aa0df80c1dc6c449018c486e4b2aa3849
kylejb <47502769+kylejb@users.noreply.github.com>
2020-12-26 16:21:36 -0500

    Fixed Breaking Change from 266161b. Linked MovieList and MovieCard to dynamically render on Search's input.

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -2,16 +2,15 @@ import MovieCard from "./MovieCard";
 
 
 const MovieList = (props: any) => {
-    // return props?.results?.map( (m: any) => {
-    //     return (
-    //             <MovieCard
-    //                 key={"ML_" + m.imdbID}
-    //                 id={"ML_" + m.imdbID}
-    //                 movie={m}
-    //             />
-    //         );   
-    //     });
-    return <p>"MovieList: MovieCards Render Below"</p>
+    return props.results?.movies.map( (m: any) => {
+        return (
+            <MovieCard
+                key={"ML_" + m.imdbID}
+                id={"ML_" + m.imdbID}
+                movie={m}
+            />
+        );   
+    });
 };
     
 export default MovieList;
