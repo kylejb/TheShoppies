d116d234c69d8184bbaedd2197378388d29fbe6f
kylejb <47502769+kylejb@users.noreply.github.com>
2020-12-26 15:27:03 -0500

    Enabled Redux DevTools.

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/index";
 
-const Store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers = composeWithDevTools({
+    // Specify name here, actionsBlacklist, actionsCreators and other options if needed
+});
+
+const Store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk),
+));
 
 export default Store;
