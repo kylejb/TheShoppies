d635a67cbf03c6d1b3946fb0a80e28e3b03b1692
kylejb <47502769+kylejb@users.noreply.github.com>
2020-12-27 01:25:30 -0500

    Successfully wired up Redux actions to add/remove.

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,3 +1,5 @@
+import { connect } from "react-redux";
+import { addMovie } from "../redux/actionCreators";
 import MovieCard from "./MovieCard";
 
 
@@ -5,12 +7,17 @@ const MovieList = (props: any) => {
     return props.results?.movies.map( (m: any) => {
         return (
             <MovieCard
-                key={"ML_" + m.imdbID}
-                id={"ML_" + m.imdbID}
+                key={m.imdbID}
+                id={m.imdbID}
                 movie={m}
+                movieHandler={props.movieHandler}
             />
         );   
     });
 };
+
+const mapDispatchToProps = ( dispatch: any ) => {
+    return { movieHandler: ( movieObj: any ) => dispatch(addMovie(movieObj))}
+};
     
-export default MovieList;
+export default connect(null, mapDispatchToProps)(MovieList);
--- src/components/NominationResults.tsx
+++ src/components/NominationResults.tsx
@@ -5,9 +5,10 @@ const NominationResults = (props: any) => {
     const renderNominated = props?.nominated?.map((movie: any) => {
         return (
             <MovieCard 
-                key={"NR_" + movie.imdbID} 
-                id={"NR_" + movie.imdbID} 
+                key={movie.imdbID} 
+                id={movie.imdbID} 
                 movie={movie} 
+                movieHandler={props.movieHandler}
             />
         );
     });
@@ -16,9 +17,7 @@ const NominationResults = (props: any) => {
     return (
         <div className="container-results">
             <h1>Nomination Results</h1>
-            <div className="moviecard">
-                {/* { props?.nominated?.length > 0 ? renderNominated : null } */}
-            </div>
+            { props.nominated ? renderNominated : null }
         </div>
     );
 };
