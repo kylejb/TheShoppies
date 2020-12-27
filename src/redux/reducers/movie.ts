e27cd60890cce28cde93f15d9d1ee3c089f24640
kylejb <47502769+kylejb@users.noreply.github.com>
2020-12-27 16:59:03 -0500

    Fixed REMOVE_MOVIE bug - replaced `id` with `imdb` in `action.payload.id`. - removed unnecessary `[]`. - replaced stringText with actionTypes for scalability.

--- src/redux/reducers/movie.ts
+++ src/redux/reducers/movie.ts
@@ -1,14 +1,17 @@
+import { ADD_MOVIE, REMOVE_MOVIE } from "../actionTypes";
+
+
 const movieReducer = (currentState: any = {nominated: []}, action: any) => {
     switch (action.type) {
-        case "ADD_MOVIE":
+        case ADD_MOVIE:
             return {
                 ...currentState, 
                 nominated: [...currentState.nominated, action.payload]
             };
-        case "REMOVE_MOVIE": 
+        case REMOVE_MOVIE:
             return {
                 ...currentState,
-                nominated: [currentState.nominated.filter( (movieObj: any) => movieObj.imdbID !== action.payload.id)]
+                nominated: currentState.nominated.filter( (movieObj: any) => movieObj.imdbID !== action.payload.imdbID)
             };
         default:
             return currentState;
