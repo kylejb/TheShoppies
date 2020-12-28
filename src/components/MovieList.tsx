64a652db013e2fcbb215baa628791a5a825ea429
kylejb <47502769+kylejb@users.noreply.github.com>
2020-12-27 19:29:52 -0500

    Revised to reincorporate conditional rendering - optimized efficiency with hashMap for O(1) lookup when iterating. - added btnDisabled and btnLabel to pass props conditionally.

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -4,16 +4,37 @@ import MovieCard from "./MovieCard";
 
 
 const MovieList = (props: any) => {
-    return props.results?.movies.map( (m: Movie) => {
-        return (
-            <MovieCard
-                key={m.imdbID}
-                movie={m}
-                movieHandler={props.movieHandler}
-            />
-        );   
-    });
+    const nominatedMap = new Map();
+    props.nominated.forEach( (m: Movie ) => nominatedMap.set(m.imdbID, m.Title));
+
+    const renderParsedMoviesAfterQuery = () => {
+        return props.results.movies.map( (m: Movie) => {
+            if (nominatedMap.get(m.imdbID)) {
+                return (
+                    <MovieCard
+                        key={m.imdbID}
+                        movie={m}
+                        movieHandler={props.movieHandler}
+                        btnDisabled={true}
+                        btnLabel="Nominate Me!"
+                    /> 
+                );
+            } else {
+                return (
+                    <MovieCard
+                        key={m.imdbID}
+                        movie={m}
+                        movieHandler={props.movieHandler}
+                        btnDisabled={false}
+                        btnLabel="Nominate Me!"
+                    />
+                );
+            };
+        });
+    };
+    return <>{renderParsedMoviesAfterQuery()}</>;
 };
+
 
 const mapDispatchToProps = ( dispatch: any ) => {
     return { movieHandler: ( movieObj: any ) => dispatch(addMovie(movieObj))}
