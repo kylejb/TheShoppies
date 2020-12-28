7b55e7879b38089858a6df03c766e59af38e9901
kylejb <47502769+kylejb@users.noreply.github.com>
2020-12-27 19:34:57 -0500

    Added connection to nominated state within Redux - passed nominated props down to MovieList through SearchResults.

--- src/containers/SearchContainer.tsx
+++ src/containers/SearchContainer.tsx
@@ -9,14 +9,14 @@ const SearchContainer = ( props: any ) => {
         <div className="wrapper-search">
             <h1>Search Container</h1>
             <Search results={props.search} />
-            <SearchResults results={props.search} />
+            <SearchResults results={props.search} nominated={props.nominated} />
         </div>
     );
 };
 
 
 const mapStateToProps = (state: any) => {
-    return { search: state.search };
+    return { search: state.search, nominated: state.movies.nominated };
 };
 
 export default connect(mapStateToProps)(SearchContainer);
