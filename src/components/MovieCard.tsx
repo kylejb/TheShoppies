6c2ae1bb3abb502e0e660eb9eb22f0e195f23985
kylejb <47502769+kylejb@users.noreply.github.com>
2020-12-29 16:31:15 -0500

    Updated CSS from v0 to v1. - Removed tooltip and displayed title clearly on Card instead. - Revised Card margins and repositioned location of icons. - Removed CSS-related comments.

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -17,12 +17,12 @@ const DivMovieCard = styled.div<MovieCardProps>`
     background-position: center; 
     background-size: cover;
     border: 1px solid transparent;
-    padding: 20px;
+    padding: 5px;
 `;
 
 const DivMovieCardTop = styled.div`
     display: flex;
-    justify-content: flex-end;
+    justify-content: center;
 `;
 const DivMovieCardBottom = styled.div`
     display: flex;
@@ -30,21 +30,22 @@ const DivMovieCardBottom = styled.div`
 `;
 
 const Label = styled.div`
-    word-break: break-all;
     display: flex;
+    word-break: break-all;
+    white-space: nowrap;
     align-items: center;
-    color: #777;
-    fill: currentColor;
     border: 1px solid transparent;
-    background-color: hsla(0,0%,100%,.85);
     border-radius: 4px;
     padding: 0 11px;
+    background-color: hsla(0,0%,100%,.85);
+    color: #777;
+    fill: currentColor;
 `;
 
 const Button = styled.button`
     display: flex;
     align-items: center;
-    margin-right: 10px;
+    margin-right: 0px;
     color: #777;
     fill: currentColor;
     border: 1px solid transparent;
@@ -53,100 +54,28 @@ const Button = styled.button`
     padding: 0 11px;
 `;
 
-const ToolTipDiv = styled.div`
-    display: inline-block;
-    border-bottom: 1px dotted black;
-
-    .tooltiptext {
-        visibility: hidden;
-        width: 120px;
-        background-color: black;
-        color: #fff;
-        text-align: center;
-        border-radius: 6px;
-        padding: 5px 0;
-        position: absolute;
-        z-index: 1;
-        bottom: 100%;
-        left: 50%;
-        margin-left: -60px;
-        
-        /* Fade in tooltip - takes 1 second to go from 0% to 100% opac: */
-        opacity: 0;
-        transition: opacity 1s;
-    }
-    
-    :hover .tooltiptext {
-        visibility: visible;
-        opacity: 1;
-    }
-`;
 
 const MovieCard = ( { movie, movieHandler, btnDisabled, btnLabel }: IMovieComProp ) => {
     return (
+        <>
         <DivMovieCard moviePosterUrl={movie.Poster}>
             <DivMovieCardTop>
-                <ToolTipDiv className="tooltip">
-                    <Label 
-                        style={{flex: 1, whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis", minWidth: 0}}
-                    >
-                        <span>{ movie.Title }</span>
+                    <Label>
+                        <span>{ movie.Title } ({movie.Year})</span>
                     </Label>
-                    <span className="tooltiptext">{ movie.Title }</span>
-                </ToolTipDiv>
-                <Label>{movie.Year}</Label>
+            </DivMovieCardTop>
+            <DivMovieCardBottom>
                 <Button
                     type="button"
                     disabled={btnDisabled}
                     onClick={() => movieHandler(movie)}
                 >
-                    <span>{ btnLabel }</span>
+                    { btnLabel }
                 </Button>
-            </DivMovieCardTop>
-            <DivMovieCardBottom>
             </DivMovieCardBottom>
         </DivMovieCard>
+            </>
     );
 };
 
 export default MovieCard;
-
-
-// justify-content: center;
-// margin-top: 0;
-// color: white;
-// background-color: black;
-
-// -webkit-box-sizing: border-box;
-// -moz-box-sizing: border-box;
-// box-sizing: border-box;
-
-// @keyframes nohovering {
-//     0%   { z-index: 9; }
-//     100% { z-index: 1; }
-// }
-
-// @keyframes hovering {
-//     0%   { z-index: 10; }
-//     100% { z-index: 10; }
-// }
-
-// -webkit-transition: all 1s ease; /* Safari and Chrome */
-// -moz-transition: all 1s ease; /* Firefox */
-// -ms-transition: all 1s ease; /* IE 9 */
-// -o-transition: all 1s ease; /* Opera */
-// transition: all 1s ease;
-
-// :hover {
-//     -webkit-transform:scale(1.25); /* Safari and Chrome */
-//     -moz-transform:scale(1.25); /* Firefox */
-//     -ms-transform:scale(1.25); /* IE 9 */
-//     -o-transform:scale(1.25); /* Opera */
-//     transform:scale(1.25);
-// }
-
-// const MoviePoster = styled.img`
-//     width: 100%;
-//     height: auto;
-//     vertical-align: middle;
-// `;
