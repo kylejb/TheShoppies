59fbd5292e450a10ed73cfa8336ddf292b75e5a4
kylejb <47502769+kylejb@users.noreply.github.com>
2020-12-29 16:56:10 -0500

    Minor formatting for code readability.

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -39,7 +39,11 @@ const MovieList = (props: any) => {
             };
         });
     };
-    return <MovieListContainer>{renderParsedMoviesAfterQuery()}</MovieListContainer>;
+    return (
+        <MovieListContainer>
+            { renderParsedMoviesAfterQuery() }
+        </MovieListContainer>
+    );
 };
 
 
