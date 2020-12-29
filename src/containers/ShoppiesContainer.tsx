c08b28b0e3b5d5a4ba6a5a3f72dff643d64b9e68
kylejb <47502769+kylejb@users.noreply.github.com>
2020-12-29 17:31:51 -0500

    Added "The Shoppies" as h1 title.

--- src/containers/ShoppiesContainer.tsx
+++ src/containers/ShoppiesContainer.tsx
@@ -5,6 +5,7 @@ import SearchContainer from "./SearchContainer";
 const ShoppiesContainer = () => {
     return (
         <div className="wrapper-shoppies">
+            <h1 style={{textAlign: "center"}}>The Shoppies</h1>
             <SearchContainer />
             <NominationContainer />
         </div>
